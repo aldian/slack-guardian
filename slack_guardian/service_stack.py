fe2f5f8fad9c3784bdaf4d469c3fb416bbeeea88
Aldian Fazrihady <aldian.f@ntucenterprise.sg>
2024-06-09 15:10:26 +0700

    Add placeholder for event processor and command handler lambdas

--- slack_guardian/service_stack.py
+++ slack_guardian/service_stack.py
@@ -10,3 +10,18 @@ from aws_cdk import (
 class SlackGuardianStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        # Lambda Functions
+        lambda_func_1 = _lambda.Function(
+            self, "EventProcessor",
+            runtime=_lambda.Runtime.PYTHON_3_10,
+            code=_lambda.Code.from_asset("lambdas"),
+            handler="event_processor.handler",
+        )
+
+        lambda_func_2 = _lambda.Function(
+            self, "CommandHandler",
+            runtime=_lambda.Runtime.PYTHON_3_10,
+            code=_lambda.Code.from_asset("lambdas"),
+            handler="command_handler.handler",
+        )
