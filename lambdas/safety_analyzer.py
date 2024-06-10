f8f3606d493fab478eb5d0b00f46e9d76e36c31a
Aldian Fazrihady <aldian.f@ntucenterprise.sg>
2024-06-10 12:18:15 +0700

    Checking safety analyzer input

--- lambdas/safety_analyzer.py
+++ lambdas/safety_analyzer.py
@@ -1,17 +1,14 @@
 import json
 import logging
 
-# Configure logging
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)  # Set the desired logging level (INFO, DEBUG, etc.)
-
 
 def handler(event, context):
+    logging.error(f"Received event: {event}")
     for record in event['Records']:
         message_body = json.loads(record['body'])
 
         # Log the message body
-        logger.info(f"Received message: {message_body}")  # Use f-string for better formatting
+        logging.error(f"Received message: {message_body}")  # Use f-string for better formatting
 
         # Perform safety analysis on the message_body
         # ...
