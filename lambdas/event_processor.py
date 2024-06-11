8d8785cb76a1d68b41e987d7fec873413568a739
Aldian Fazrihady <aldian.f@ntucenterprise.sg>
2024-06-12 05:24:44 +0700

    Experiment with action

--- lambdas/event_processor.py
+++ lambdas/event_processor.py
@@ -58,8 +58,8 @@ def message_hello(message, say):
 @app.action("button_click")
 def action_button_click(body, ack, say):
     # Acknowledge the action
-    ack()
     say(f"<@{body['user']['id']}> clicked the button")
+    ack()
 
 
 def handler(event, context):
